be750dfae0d8387088eb473010e57567551ae7f8
Rajaram Rabindranath <rajaram.rabindranath@gmail.com>
2015-04-25 06:19:46 -0400

    DBops cleaned up

--- src/edu/buffalo/cse/ambience/database/DBOps.java
+++ src/edu/buffalo/cse/ambience/database/DBOps.java
@@ -9,13 +9,9 @@ import edu.buffalo.cse.ambience.dataStructures.gyan;
 
 public interface DBOps 
 {
-	public boolean createTable(String tableName,String[] colfams);
-	public boolean readTable(String tableName,String colfam);
-	public boolean loadData(String tableName,Columns c,Rows r, String[] colFam);
 	public boolean add();
 	public boolean delete();
 	public boolean modify();
 	public boolean scan();
-	public ArrayList<gyan> topT(int T,int korder);
-	public ArrayList<gyan> topT(String tblname,int T,int korder,Order sortOrder);
+	public ArrayList<gyan> topT(String tblname,int T,int korder);
 }
