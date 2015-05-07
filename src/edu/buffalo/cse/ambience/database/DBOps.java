f71c34b3eb86cb1850042dc674db2658f5a1f742
Rajaram Rabindranath <rajaram.rabindranath@gmail.com>
2015-05-07 06:27:58 -0400

    Important interface changes

--- src/edu/buffalo/cse/ambience/database/DBOps.java
+++ src/edu/buffalo/cse/ambience/database/DBOps.java
@@ -1,12 +1,5 @@
 package edu.buffalo.cse.ambience.database;
 
-import java.util.ArrayList;
-
-import orderly.Order;
-import edu.buffalo.cse.ambience.dataStructures.Columns;
-import edu.buffalo.cse.ambience.dataStructures.ContingencyT;
-import edu.buffalo.cse.ambience.dataStructures.Rows;
-import edu.buffalo.cse.ambience.dataStructures.gyan;
 
 public interface DBOps 
 {
@@ -14,10 +7,5 @@ public interface DBOps
 	public boolean delete();
 	public boolean modify();
 	public boolean scan();
-	public ArrayList<gyan> topT(String tblname,int T,int korder);
-	public double computeKWII(ContingencyT cTbl);
-	public double computePAI(ContingencyT cTble);
-	public ContingencyT getCTable(gyan g);
-	public ArrayList<Double> computeKWII(ArrayList<gyan> listg);
-	public double computeEntropy(gyan g);
+	public void tableDump(String tbl,String dumpName,String colfam,String qual);
 }
